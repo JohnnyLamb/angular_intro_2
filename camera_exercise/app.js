39c14ebe10b9db6ce0876f676d60dd6398cb5964
johnny lamb <agnusjack@gmail.com>
2015-09-14 13:47:58 -0600

    adding the cameras

--- camera_exercise/app.js
+++ camera_exercise/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("myapp", []);
+var app = angular.module("myApp", []);
       app.controller("MainController", ['$scope',function($scope) {
           $scope.hello = {};
           $scope.hello.title = "Worldy yay";
@@ -7,6 +7,29 @@ var app = angular.module("myapp", []);
 
           $scope.dupes =[1,1,2,5,6,9,9,9];
 
+          $scope.cameras =[
+            {
+              title: "Nikon D3100 DSLR",
+              image: "http://ecx.images-amazon.com/images/I/713u2gDQqML._SX522_.jpg",
+              rating: 3.4,
+              price: 369.99,
+              onSale: true
+            },
+            {
+              title: "Canon EOS 70D",
+              image: "http://ecx.images-amazon.com/images/I/81U00AkAUWL._SX522_.jpg",
+              rating: 2.0,
+              price: 1099.0,
+              onSale: false
+            },
+            {
+              title: "Nikon D810A",
+              image:"http://ecx.images-amazon.com/images/I/91wtXIfLl2L._SX522_.jpg",
+              rating: 4.2,
+              price: 3796.95,
+              onSale: true
+            }
+          ];
+
+
       }]);
-
-
