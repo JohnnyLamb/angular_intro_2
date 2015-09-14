1681ce2f99bd18c04792eec4fcba8b2530869f9c
johnny lamb <agnusjack@gmail.com>
2015-09-14 11:56:24 -0600

    camera exercise

---
+++ madlibs/app.js
@@ -0,0 +1,12 @@
+var app = angular.module("myapp", []);
+      app.controller("HelloController", function($scope) {
+        $scope.hello = {};
+        $scope.hello.title = "Worldy yay";
+      });
+
+      // app.controller('expressions',function($scope){
+      //   $scope.add = {};
+      //   $scope.add.
+
+      // });
+
