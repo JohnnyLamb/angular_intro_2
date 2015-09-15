2c94dbfddc7111e4e0d30cf372c478be95df5304
johnny lamb <agnusjack@gmail.com>
2015-09-15 16:33:17 -0600

    adding reddit clone

---
+++ reddit/app.js
@@ -0,0 +1,12 @@
+var app = angular.module("myapp", []);
+      app.controller("RedditController", function($scope) {
+        $scope.hello = {};
+        $scope.hello.title = "Worldy yay";
+      });
+
+      // app.controller('expressions',function($scope){
+      //   $scope.add = {};
+      //   $scope.add.
+
+      // });
+
